3231af520387e27df9501ac81da665cc04206111
nexxgodd <crazy_nuts_2006@yahoo.com>
2021-08-30 11:07:37 -0500

    changed icons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {NgxChessBoardService} from 'ngx-chess-board';
+import {NgxChessBoardService, PieceIconInput} from 'ngx-chess-board';
 import {NgxChessBoardView} from 'ngx-chess-board';
 import Game from '../models/game';
 @Component({
@@ -9,7 +9,23 @@ import Game from '../models/game';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(private boardService: NgxChessBoardService, private http: HttpClient) { }
+	icons:PieceIconInput;
+  constructor(private boardService: NgxChessBoardService, private http: HttpClient) {
+		this.icons = {
+        blackBishopUrl: 'assets/img/bB.png',
+        blackKingUrl: 'assets/img/bK.png',
+        blackKnightUrl: 'assets/img/bN.png',
+        blackPawnUrl: 'assets/img/bP.png',
+        blackQueenUrl: 'assets/img/bQ.png',
+        blackRookUrl: 'assets/img/bR.png',
+        whiteBishopUrl: 'assets/img/wB.png',
+        whiteKingUrl: 'assets/img/wK.png',
+        whiteKnightUrl: 'assets/img/wN.png',
+        whitePawnUrl: 'assets/img/wP.png',
+        whiteQueenUrl: 'assets/img/wQ.png',
+        whiteRookUrl: 'assets/img/wR.png'
+    };
+   }
   @ViewChild('board', {static: false}) board!: NgxChessBoardView;
   ngOnInit(): void {
     
