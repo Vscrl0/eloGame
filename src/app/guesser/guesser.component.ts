02da6754256a1622597682fb35903427a5173f6b
Todd <nexxgodd@gmail.com>
2021-09-02 16:43:57 -0500

    next game

--- src/app/guesser/guesser.component.ts
+++ src/app/guesser/guesser.component.ts
@@ -2,25 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { EloService } from '../services/elo.service';
 
 @Component({
-  selector: 'app-guesser',
-  templateUrl: './guesser.component.html',
-  styleUrls: ['./guesser.component.css']
+	selector: 'app-guesser',
+	templateUrl: './guesser.component.html',
+	styleUrls: ['./guesser.component.css']
 })
 export class GuesserComponent implements OnInit {
-  avgElo = 0;
-  score = 0;
-  guess = 0;
-  constructor(private eloService: EloService) { }
+	avgElo = 0;
+	score = -1;
+	guess = 0;
+	totalScore = 0;
+	constructor(private eloService: EloService) { }
 
-  ngOnInit(): void {
-	if(!this.eloService.gameCount){
-		this.eloService.goHome();
+	ngOnInit(): void {
+		if (!this.eloService.gameCount) {
+			this.eloService.goHome();
+		}
 	}
-  }
-  ngDoCheck(): void {
-    this.eloService.getElo$.subscribe( data => {this.avgElo = data as number;})
-  }
-  setScore(elo: number) {
-    this.score = Math.max(0,2000 - Math.abs(elo - this.avgElo));
-  }
+	ngDoCheck(): void {
+		this.eloService.getElo$.subscribe(data => { this.avgElo = data as number; })
+	}
+	setScore(elo: number) {
+		this.score = Math.max(0, 2000 - Math.abs(elo - this.avgElo));
+		this.totalScore += this.score;
+	}
+
+	nextGame() {
+		this.score = -1;
+		this.eloService.nextGame();
+	}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,6 +19,7 @@ export class HomeComponent implements OnInit {
 	pgnSplitWithoutNums!: Array<string>;
 	pgnScore = "";
 	avgElo!: number;
+	M=Math;
 
 	constructor(private boardService: NgxChessBoardService, public eloService: EloService) {
 		this.icons = {
@@ -37,8 +38,6 @@ export class HomeComponent implements OnInit {
 		};
 	}
 	@ViewChild('board', { static: false }) board!: NgxChessBoardView;
-	// ngOnInit(): void {
-	// }
 	ngAfterViewInit() {
 		// this.reset();
 		this.board.setFEN("8/8/p2p1QNK/n2r2R1/bqkp2B1/r2b2N1/p2n1BPR/8 w - - 0 1");
